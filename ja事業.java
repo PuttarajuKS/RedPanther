d1fbf0de7b1ab266a0967f25a79aaf6644c6f7c6
PuttarajuKS <88081171+PuttarajuKS@users.noreply.github.com>
2022-05-19 14:22:46 +0530

    new ja file added

---
+++ ja事業.java
@@ -0,0 +1,5 @@
+PULSE CONFIGURATION
+  
+    When working with the Pulse web application, refer to the following glossary:
+事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業
+事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業事業
